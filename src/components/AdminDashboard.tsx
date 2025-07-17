9376ba1934e256deddd491475d15dd5fa46dfe63
RisingLanguages <risinglanguages.contact@gmail.com>
2025-07-17 11:15:30 +0200

    Updated AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -39,7 +39,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 
 import { auth, db } from '../firebase/config';
 import { Application } from '../types';
-import { format } from 'date-fns';
+
 import { arSA } from 'date-fns/locale';
 
 import WorkshopManagement from './WorkshopManagement';
